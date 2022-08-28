82e84e0c5b84e3fbf627cbe3982cf7548b38c97e
Namik <namazov306@gmail.com>
2022-08-29 00:34:50 +0300

    Configure xib file for cells in feed view controller, add post label, name label, avatar image, likes, comments, share and views, populate with this table view, refactoring

--- VkClone/VkClone/Sources/NewsFeed/Cell/NewsFeedTableViewCell.swift
+++ VkClone/VkClone/Sources/NewsFeed/Cell/NewsFeedTableViewCell.swift
@@ -10,6 +10,14 @@ import UIKit
 final class NewsFeedTableViewCell: UITableViewCell {
     static let identifier = "NewsFeedTableViewCell"
     
+    @IBOutlet weak var viewsCountLabel: UILabel!
+    @IBOutlet weak var shareCountLabel: UILabel!
+    @IBOutlet weak var commentCountLabel: UILabel!
+    @IBOutlet weak var likesCountLabel: UILabel!
+    @IBOutlet weak var postLabel: UILabel!
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     override class func awakeFromNib() {
         super.awakeFromNib()
     }
