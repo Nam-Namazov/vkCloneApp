2bfc440736963ea815a2952ec43933cbe7def29d
Namik <namazov306@gmail.com>
2022-10-06 20:36:58 +0300

    Refactoring, implement moreTextButton with delegation

--- VkClone/VkClone/Sources/NewsFeed/NewsFeedModels.swift
+++ VkClone/VkClone/Sources/NewsFeed/NewsFeedModels.swift
@@ -8,18 +8,45 @@
 
 import UIKit
 
+protocol FeedCellViewModel {
+    var iconUrlString: String { get }
+    var name: String { get }
+    var date: String { get }
+    var text: String? { get }
+    var likes: String? { get }
+    var comments: String? { get }
+    var shares: String? { get }
+    var views: String? { get }
+    var photoAttachement: FeedCellPhotoAttachementViewModel? { get }
+    var sizes: FeedCellSizes { get }
+}
+
+protocol FeedCellSizes {
+    var postLabelFrame: CGRect { get }
+    var attachmentFrame: CGRect { get }
+    var bottomViewFrame: CGRect { get }
+    var totalHeight: CGFloat { get }
+    var moreTextButtonFrame: CGRect { get }
+}
+
+protocol FeedCellPhotoAttachementViewModel {
+    var photoUrlString: String? { get }
+    var width: Int { get }
+    var height: Int { get }
+}
+
 enum NewsFeed {
-    
     enum Model {
         struct Request {
             enum RequestType {
                 case getNewsFeed
+                case revealPostIds(postId: Int)
             }
         }
         
         struct Response {
             enum ResponseType {
-                case presentNewsFeed(feed: FeedResponse)
+                case presentNewsFeed(feed: FeedResponse, revealdedPostIds: [Int])
             }
         }
         
@@ -34,6 +61,7 @@ enum NewsFeed {
 struct FeedViewModel {
     
     struct Cell: FeedCellViewModel {
+        var postId: Int
         var iconUrlString: String
         var name: String
         var date: String
@@ -42,7 +70,7 @@ struct FeedViewModel {
         var comments: String?
         var shares: String?
         var views: String?
-        var photoAttachment: FeedCellPhotoAttachementViewModel?
+        var photoAttachement: FeedCellPhotoAttachementViewModel?
         var sizes: FeedCellSizes
     }
     
